e81da964bd234e83adf7d310607d177451536274
joris hechtermans <jorishechtermans@ControleKabutor.lan>
2021-12-07 10:58:11 +0100

    added HRM & logo

--- app/pages/measure_heartbeat.js
+++ app/pages/measure_heartbeat.js
@@ -1,7 +1,10 @@
 import document from "document";
 import { switchPage } from "../navigation";
+import { HeartRateSensor } from "heart-rate";
+import clock from "clock";
 
 let buttonGenreSelection = null;
+let heartRateHandle = "--";
 
 export function destroy() {
   console.log("destroy heartbeat page");
@@ -11,6 +14,15 @@ export function destroy() {
 export function init() {
   console.log("init heartbeat page");
   buttonGenreSelection = document.getElementById('genre-button');
+  heartRateHandle = document.getElementById("heartRateLabel");
+
+  let hrm = new HeartRateSensor();
+
+  hrm.onreading = function() {
+    console.log('Current heart rate: ' + `${hrm.heartRate}` )
+    heartRateHandle.text = `${hrm.heartRate}`;
+  }
+  hrm.start();
 
   buttonGenreSelection.onclick = () => {
     switchPage('genre_selection');
